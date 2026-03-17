232e7ed7a331bf832bf56654d2cf40cc42196413
”abdelrahman <eabdo6990@gmail.com>
2026-03-17 12:18:14 +0200

    project structure

---
+++ src/app.controller.js
@@ -0,0 +1,30 @@
+import dbconeection from "./DB/db.connection.js"
+//import authroutes from '../src/moduels/auth/auth.routes.js'
+
+
+
+import cors from 'cors'
+
+
+const bootstrap =(app,express)=>{
+    app.use(express.json())
+    app.use(cors())
+
+
+    //app.use('/auth',authroutes)
+
+
+
+
+ app.use("*",(req,res,next)=>{
+    return res.status(404).json({message:"invalid routing"})
+
+
+ })
+
+ dbconeection()
+
+}
+
+
+export default bootstrap
