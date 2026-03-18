386839d5cbe4c005b360a7161548a9742456f8c9
”abdelrahman <eabdo6990@gmail.com>
2026-03-18 23:46:22 +0200

    add some if hte user feature

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken"
+
+export const auth = (req, res, next) => {
+    const {authorization} = req.headers
+    if (!authorization)
+        return res.status(401).json({ message: "No token provided" })
+    const token = authorization.split(" ")[1]
+    try {
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SIGNATURE)
+        req.user = decoded;
+        req.userID = decoded.id;
+        next()
+    } catch (error) {
+        return res.status(401).json({ message: "Invalid token" ,message:error.message,error,stack:error.stack})
+    }
+}
+
+
